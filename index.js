35dbdf925806330a7d8e2d4b55d18d3e92e09400
priyanka1971 <104821422+priyanka1971@users.noreply.github.com>
2023-07-04 09:39:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
-let slides = document.querySelectorAll('.slidecontainer');
-let index=0;
-function next(){
-    slides[index].ClassList.remove('active');
-    index = (index + 1) % slides.length;
-    slides[index].ClassList.add('active');
-}
-function prev (){
-    slides[index].ClassList.remove('active');
-    index = (index - 1 + slides.length) % slides.length;
-    slides[index].ClassList.add('active');
+
+
+window.addEventListener('scroll',reveal);
+
+function reveal(){
+   var reveals=document.querySelectorAll('.reveal');
+   for (var i=0; i < reveals.length;i++)
+   {
+    var windowheight = window.innerHeight;
+    var revealtop = reveals[i].getBoundingClientRect().top;
+    var revealpoint =150;
+    if(revealtop < windowheight-revealpoint)
+    {
+        reveals[i].classList.add('active');
+   }
+   else{
+    reveals[i].classList.remove('active');
+   }
+ }
 }
 
-setInterval (next,5000);
+// achievement js
+let valueDisplays = document.querySelectorAll(".num");
+let interval = 4000;
+
+valueDisplays.forEach((valueDisplay) => {
+    let startValue = 0;
+    let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+    let duration = Math.floor(interval / endValue);
+    let counter = setInterval(function(){
+        startValue += 1;
+        valueDisplay.textContent = startValue;
+        if(startValue == endValue){
+            clearInterval(counter);
+        }
+    },duration);
+});
